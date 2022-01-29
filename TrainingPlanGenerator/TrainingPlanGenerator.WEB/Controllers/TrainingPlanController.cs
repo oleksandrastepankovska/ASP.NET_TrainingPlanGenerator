e6ec6b5a1696a202e9ca5a40bb84a034e96abcd4
oleksandrastepankovska <oleksandra.st3@gmail.com>
2022-01-29 16:26:10 +0200

    updated training plan page

--- TrainingPlanGenerator/TrainingPlanGenerator.WEB/Controllers/TrainingPlanController.cs
+++ TrainingPlanGenerator/TrainingPlanGenerator.WEB/Controllers/TrainingPlanController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TrainingPlanGenerator.Core.Interfaces;
 using TrainingPlanGenerator.Core.ProjectAggregate.Entities;
@@ -10,22 +11,39 @@ namespace TrainingPlanGenerator.Web.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IRepository<TrainingPlan> _trainingPlanRepository;
+        private readonly IRepository<AppUser> _userRepository;
+        private readonly UserManager<IdentityUser> _userManager;
 
         public TrainingPlanController(
             IMapper mapper,
-            IRepository<TrainingPlan> trainingPlanRepository
+            IRepository<TrainingPlan> trainingPlanRepository,
+            IRepository<AppUser> userRepository,
+            UserManager<IdentityUser> userManager
             )
         {
             _mapper = mapper;
             _trainingPlanRepository = trainingPlanRepository;
+            _userRepository = userRepository;
+            _userManager = userManager;
         }
 
         [HttpGet("trainingplan")]
         public async Task<IActionResult> Index(int id)
         {
+            var pageViewModel = new TrainingPlanPageViewModel();
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var idenityUser = await _userManager.GetUserAsync(User);
+                var user = await _userRepository.GetSingleAsync(x => string.Equals(idenityUser.Id, x.IdentityUserId));
+
+                pageViewModel.User = _mapper.Map<UserViewModel>(user);
+            }
+
             var trainingPlan = await _trainingPlanRepository.GetSingleAsync(x => x.Id == id, x => x.Excersises);
-            var trainingPlanViewModel = _mapper.Map<TrainingPlanViewModel>(trainingPlan);
-            return View(trainingPlanViewModel);
+            pageViewModel.TrainingPlan = _mapper.Map<TrainingPlanViewModel>(trainingPlan);
+
+            return View(pageViewModel);
         }
     }
 }
